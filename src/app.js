063eae6c77a41e0fb47025cab8d903d61d02833a
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-25 15:14:29 -0300

    Created the routes.js and configurated the application to ejs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("/");
-});
+const routes = require("./routes");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(3000, () => {
     console.log("Running...");
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const routes = express.Router();
+
+routes.get("/", (req, res) => {
+    res.render("index");
+});
+
+routes.get("/create", (req, res) => {
+    res.render("create-pass");
+});
+
+routes.get("/room", (req, res) => {
+    res.render("room");
+});
+
+module.exports = routes;
