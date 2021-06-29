23ebf8413665d88bbcf7ac436d3f81206901a558
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-29 09:14:11 -0300

    Created QuestionController

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const QuestionController = require("./controllers/QuestionController");
+
 const routes = express.Router();
 
 routes.get("/", (req, res) => res.render("index"));
@@ -8,6 +10,6 @@ routes.get("/create-pass", (req, res) => res.render("create-pass"));
 
 routes.get("/room", (req, res) => res.render("room"));
 
-routes.post("/room/:room/:question/:action");
+routes.post("/room/:room/:question/:action", QuestionController.index);
 
 module.exports = routes;
