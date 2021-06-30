e792b353ae2d096cb5a2149977eda1b3cca33dd9
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-30 14:05:28 -0300

    Finished the database configuration

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -2,6 +2,8 @@ module.exports = {
     create(req, res) {
         let roomId = 123456;
 
+        
+
         res.redirect(`/room/${roomId}`);
     }
 }
---
+++ src/database/config.js
@@ -0,0 +1,9 @@
+const sqlite3 = require("sqlite3");
+
+const { open } = require("sqlite");
+
+module.exports = () => 
+    open ({
+        filename: "/src/database/rocketq.sqlite",
+        driver: sqlite3.Database,
+    })
