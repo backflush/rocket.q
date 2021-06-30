9dd0a5b054bed6f0c995188e5cc07aaf52fb9ff7
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-30 13:50:31 -0300

    Fixed the corrupted files

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const QuestionController = require("./controllers/QuestionController");
+const RoomController = require("./controllers/RoomController");
 
 const routes = express.Router();
 
@@ -8,8 +9,10 @@ routes.get("/", (req, res) => res.render("index", { page: "enter-room" }));
 
 routes.get("/create-pass", (req, res) => res.render("index", { page: "create-pass" }));
 
-routes.get("/room", (req, res) => res.render("room"));
+routes.get("/room/:room", (req, res) => res.render("room"));
 
 routes.post("/room/:room/:question/:action", QuestionController.index);
+
+routes.post("/room/create-room", RoomController.create);
 
 module.exports = routes;
