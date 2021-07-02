9da17803589462a0b4acdd5556a98c6e9cc3d6d2
Wesley Bernardes <bwesleysouza@gmail.com>
2021-07-02 11:59:52 -0300

    Finished the RoomController creation

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,8 +1,27 @@
-module.exports = {
-    create(req, res) {
-        let roomId = 123456;
+const database = require("../database/config");
 
-        
+module.exports = {
+    async create(req, res) {
+        const db = await database;
+
+        const password = req.body.password;
+
+        let roomId;
+
+        for(let counter = 0; counter < 6; counter++) {
+            counter == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString();
+        }
+
+        await db.run(`
+            INSERT INTO rooms (
+                id,
+                password
+            ) VALUES (
+                 
+            )
+        `);
+
+        await db.close();
 
         res.redirect(`/room/${roomId}`);
     }
