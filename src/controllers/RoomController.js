03e6efff31822a9795e76fd009fcbf133b43cc50
Wesley Bernardes <bwesleysouza@gmail.com>
2021-07-05 12:03:15 -0300

    Finished the 4th day

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -2,24 +2,34 @@ const database = require("../database/config");
 
 module.exports = {
     async create(req, res) {
-        const db = await database;
+        const db = await Database();
 
         const password = req.body.password;
 
         let roomId;
+        let isRoom = true;
 
-        for(let counter = 0; counter < 6; counter++) {
-            counter == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString();
+        while(isRoom) {
+            for(let counter = 0; counter < 6; counter++) {
+                counter == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString();
+            }
+    
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+    
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId);
+    
+            if(!isRoom) {
+                await db.run(`
+                    INSERT INTO rooms (
+                        id,
+                        password
+                    ) VALUES (
+                        ${parseInt(roomId)},
+                        ${password}
+                    )
+                `);
+            }
         }
-
-        await db.run(`
-            INSERT INTO rooms (
-                id,
-                password
-            ) VALUES (
-                 
-            )
-        `);
 
         await db.close();
 
